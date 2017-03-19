a6ae756da18c92f090f6de42231ec78d01b32d41
Sofiia Pasichnyk <sofiia.pasichnyk@smartbear.com>
2017-03-19 17:27:05 +0100

    add echarts-for-react in chart view

--- src/routes/Chart/components/Chart.js
+++ src/routes/Chart/components/Chart.js
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactEcharts from 'echarts-for-react';
 
-export const Counter = (props) => (
-  <div style={{ margin: '0 auto' }} >
-    <h2>Counter: {props.counter}</h2>
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
-  </div>
-)
+export class Chart extends Component {
 
-Counter.propTypes = {
-  counter     : React.PropTypes.number.isRequired,
-  doubleAsync : React.PropTypes.func.isRequired,
-  increment   : React.PropTypes.func.isRequired
+  render() {
+    const option = {
+      //from https://ecomfe.github.io/echarts-examples/public/editor.html?c=pie-custom
+
+      title: {
+        text: 'My Pie (go to Counter tab to change your value)',
+        left: 'center',
+        top: 20
+      },
+
+      tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      series: [
+        {
+          name: 'Pie',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '50%'],
+          data: [
+            { value: this.props.counter, name: 'Your value' },
+            { value: 3, name: 'Constant 3' },
+            { value: 5, name: 'Constant 5' }
+          ].sort(function (a, b) {
+            return a.value - b.value
+          })
+        }
+      ]
+    };
+
+    return (
+      <div
+        style={{ margin: '0 auto' }}>
+        <ReactEcharts
+          option={option}
+          notMerge={true}
+          lazyUpdate={true}
+        />
+      </div>
+    )
+  }
 }
 
-export default Counter
+export default Chart
