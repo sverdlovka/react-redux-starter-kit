c03340fc20a46c422c80a1e019f57f5a2f9bde8c
Sofiia Pasichnyk <sofiia.pasichnyk@smartbear.com>
2017-03-19 15:40:40 +0100

    add route for chart view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,10 @@ export const Header = () => (
     <Link to='/counter' activeClassName='route--active'>
       Counter
     </Link>
+    {' · '}
+    <Link to='/chart' activeClassName='route--active'>
+      Chart
+    </Link>
   </div>
 )
 
